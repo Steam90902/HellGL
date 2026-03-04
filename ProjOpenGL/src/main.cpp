a77f81fa8af50f3774710ca5a451ec85d576602c
Steam90902 <gabassumani@gmail.com>
2026-03-04 17:03:19 +0000

    Turned it into a function, im so cool

--- ProjOpenGL/src/main.cpp
+++ ProjOpenGL/src/main.cpp
@@ -15,7 +15,7 @@
 void processInput(GLFWwindow *window);
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 
-std::pair<std::vector<GLfloat>, std::vector<GLuint>> circleCoords(float radius);
+VAO circleCoords(float radius);
 
 GLFWwindow* StartGLFW();
 
@@ -28,13 +28,7 @@ int main() {
     //making the points of the triangle
     //we use a range of -1 to 1, with the limit of the range being the border of the window
     //also, as openGL is a 3d renderer, the third coordinate is the z axis.
-    std::pair<std::vector<GLfloat>, std::vector<GLuint>> temp = circleCoords(0.5f);
 
-
-    std::vector<GLfloat> points = temp.first;
-
-
-    std::vector<GLuint> indices = temp.second;
 
     //just error checks and for debugging
     GLFWwindow* window = StartGLFW();
@@ -49,60 +43,56 @@ int main() {
     // EBO for Element Buffer Object
 
     //bind the variables
-    VAO VAO1;
-    VAO1.Bind();
+    //VAO VAO1;
+    //VAO1.Bind();
 
-    VBO VBO1(points.data(), points.size() * sizeof(GLfloat));
-    EBO EBO1(indices.data(), indices.size() * sizeof(GLuint));
+    //VBO VBO1(points.data(), points.size() * sizeof(GLfloat));
+    //EBO EBO1(indices.data(), indices.size() * sizeof(GLuint));
 
 
     //link vao to vbo
-    VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 6 * sizeof(float), (void*)0);
-    VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    //VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 6 * sizeof(float), (void*)0);
+    //VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 6 * sizeof(float), (void*)(3 * sizeof(float)));
 
     //then unbind the variables
-    VAO1.Unbind();
-    VBO1.Unbind();
-    EBO1.Unbind();
+    //VAO1.Unbind();
+    //VBO1.Unbind();
+    //EBO1.Unbind();
 
-
+    VAO circleVAO = circleCoords(0.1f);
 
     glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
     //finding the amount of vertices
-    int vertAmount = points.size() / sizeof(GLfloat);
+    //int vertAmount = points.size() / sizeof(GLfloat);
 
     while (!glfwWindowShouldClose(window)) {
         //input
         processInput(window);
 
-        //funny
+        //cool bgd
         glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
         shaderProgram.Activate();
-        VAO1.Bind();
+        circleVAO.Bind();
         // I wanna make circles work here as a function
-        glDrawArrays(GL_TRIANGLE_FAN, 0, points.size());
+        glDrawArrays(GL_TRIANGLE_FAN, 0, 20 * 6);
         //check for events
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
     //delete the variable so no wasted mem
-    VAO1.Delete();
-    VBO1.Delete();
-    EBO1.Delete();
+    circleVAO.Delete();
+    //VBO1.Delete();
+    //EBO1.Delete();
     shaderProgram.Delete();
-
-
-
 
 
     glfwDestroyWindow(window);
     glfwTerminate();
     return 0;
-    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
 }
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
@@ -131,7 +121,7 @@ GLFWwindow *StartGLFW()
     glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, true);
 
 
-    GLFWwindow* window = glfwCreateWindow(800, 800, "Particle Hopeful", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(800, 800, "Particle Hopeful", nullptr, nullptr);
 
     if (window == nullptr) {
         std::cout << "Failed to create GLFW window" << std::endl;
@@ -150,42 +140,40 @@ GLFWwindow *StartGLFW()
     return window;
 }
 
-
-std::pair<std::vector<GLfloat>, std::vector<GLuint>> circleCoords(float radius) {
+VAO circleCoords(float radius) {
     const int steps = 20;
     const float angle = 2.0f * M_PI / steps;
     GLfloat color[] = {1.0f, 0.0f, 0.0f};
-    std::vector<GLfloat> newCoords;
-    newCoords.reserve((steps + 1) * 6);
+    std::vector<GLfloat> points;
+    points.reserve((steps + 1) * 6);
 
-    newCoords.push_back(0.0f);
-    newCoords.push_back(0.0f);
-    newCoords.push_back(0.0f);
-    for (GLfloat c : color) newCoords.push_back(c);
+    points.push_back(0.0f);
+    points.push_back(0.0f);
+    points.push_back(0.0f);
+    for (GLfloat c : color) points.push_back(c);
 
 
 
     for (int i=0; i<=steps; i++) {
         float newX = sin(angle*i) * radius;
         float newY = cos(angle*i) * radius;
-        newCoords.push_back(newX);
-        newCoords.push_back(newY);
-        newCoords.push_back(0.0f);
+        points.push_back(newX);
+        points.push_back(newY);
+        points.push_back(0.0f);
         //might be a bug here, idk y tho
         for ( GLfloat ic: color) {
-            newCoords.push_back(ic);
+            points.push_back(ic);
         }//repeat of before
     }
 
+    VAO VAO1;
+    VAO1.Bind();
 
-    std::vector<GLuint> indices;
-    indices.reserve((steps) * 3);
+    VBO VBO1(points.data(), points.size() * sizeof(GLfloat));
+    VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 6 * sizeof(float), (void*)nullptr);
+    VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    VAO1.Unbind();
+    VBO1.Unbind();
 
-    for (int i=1; i<steps; i++) {
-        indices.push_back(0);
-        indices.push_back(i);
-        indices.push_back(i+1);
-    }
-
-    return {newCoords, indices};
+    return VAO1;
 }
