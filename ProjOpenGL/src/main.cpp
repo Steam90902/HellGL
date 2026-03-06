1f235c3e5df4a0785467c489bd2c375b36d4bd04
Steam90902 <gabassumani@gmail.com>
2026-03-06 08:44:55 +0000

    Function

--- ProjOpenGL/src/main.cpp
+++ ProjOpenGL/src/main.cpp
@@ -155,6 +155,7 @@ GLFWwindow *StartGLFW()
         return nullptr;
     }
 
+
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
